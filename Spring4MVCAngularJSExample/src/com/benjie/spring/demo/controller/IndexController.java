49ae942e61eafd0181ff60d40e7b7a706cf385ae
webrow32 <benjie_rusia@yahoo.com>
2016-06-30 14:29:39 +0800

    Add files via upload

---
+++ Spring4MVCAngularJSExample/src/com/benjie/spring/demo/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.benjie.spring.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String getIndexPage(ModelMap model) {
+		return "UserManagement";
+	}
+
+}
